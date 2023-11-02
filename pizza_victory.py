e516e7380a23c71b21e3cec0f32aeb7d498fc6a7
arfendra <149658522+arfendra@users.noreply.github.com>
2023-11-02 14:27:30 +0700

    Update pizza_victory.py

--- pizza_victory.py
+++ pizza_victory.py
@@ -22,3 +22,25 @@ elif pesan_pizza == 4:
 else:
     print("Pilihan menu pizza tidak valid.")
     exit()
+#Silahkan dipilih crust pizza  
+print("\nMenu Crust Pizza:")              
+print("1. Pan Rp 0 ")
+print("2. Cheesy bite Rp 10,000 ")
+print("3. Stuffed Rp 15,000 ")
+print("4. Crown Rp 18,000 ")
+
+#input tambahan crust
+crust_pizza = int(input("Pilih crust pizza [1/2/3/4]:"))
+
+#pesan crust
+if crust_pizza == 1 : 
+    harga_crust = 0
+elif crust_pizza == 2 : 
+    harga_crust =  10000
+elif crust_pizza == 3 : 
+    harga_crust = 15000
+elif crust_pizza == 4 :
+    harga_crust = 18000   
+else:
+    print("Pilihan crust tidak valid.")    
+    exit()
